1033658f540a9f264cfd119e9680b8e73f8054a1
Nate <foxyfan23@gmail.com>
2024-07-13 10:42:00 -0500

    seed.js file fully updated

--- starter-template/prisma/seed.js
+++ starter-template/prisma/seed.js
@@ -2,21 +2,30 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const todo = [
+  {
+    id: 1,
+    name: "Teeth brushing",
+    title: "Brush your teeth",
+    complete: false
+  },
+  {
+    id: 2,
+    name: "Get dressed",
+    title: "Get dressed",
+    complete: true
+  }
+]
+
 async function main() {
-  const todo = [
-    {
-      id: 1,
-      name: "Teeth brushing",
-      title: "Brush your teeth",
-      complete: false
-    },
-    {
-      id: 2,
-      name: "Get dressed",
-      title: "Get dressed",
-      complete: true
-    }
-  ]
+  console.log(`Start seeding ...`);
+  for (const m of todo) {
+    const todoItem = await prisma.menu.create({
+      data: m,
+    });
+    console.log(`Created menu item with id: ${menuItem.id}`);
+  }
+  console.log(`Seeding finished.`);
 }
 
 main()
