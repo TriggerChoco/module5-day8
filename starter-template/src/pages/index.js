95acf61554fbff77bbb80097a6ebdcbe11510774
Nate <foxyfan23@gmail.com>
2024-07-13 11:13:36 -0500

    todos appear on page

--- starter-template/src/pages/index.js
+++ starter-template/src/pages/index.js
@@ -1,63 +1,29 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [menu, setMenu] = useState([]);
+  const [todo, setTodo] = useState([]);
 
   useEffect(() => {
     async function loadData() {
-      const res = await fetch("/api/menu");
+      const res = await fetch("/api/todo");
       const data = await res.json();
-      setMenu(data.menu);
+      setTodo(data.todo);
     }
     loadData();
   }, []);
 
-  function addToCart(id) {
-    fetch(`/api/cart`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id: id, quantity: 1 }),
-    });
-  }
-
-  if (menu.length === 0) {
-    return <h1>Loading...</h1>;
-  }
-
   return (
     <>
-      <h1>Welcome to Coffee Shop</h1>
-      <h4>Here is our menu:</h4>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          padding: "10px",
-        }}
-      >
-        {menu.map((item) => {
-          return (
-            <div
-              key={item.id}
-              style={{
-                width: "300px",
-                border: "1px solid black",
-                borderRadius: "10px",
-                margin: "10px",
-                padding: "10px",
-              }}
-            >
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-              <p>{item.price}</p>
-              <button onClick={() => addToCart(item.id)}>Add to cart</button>
-            </div>
-          );
-        })}
-      </div>
+      <h1>Welcome to the Todo List</h1>
+      <h2>Here are your todos</h2>
+      {todo.map((item) => {
+        return (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <h3>{item.completed}</h3>
+          </div>
+        )
+      })}
     </>
   );
 }
