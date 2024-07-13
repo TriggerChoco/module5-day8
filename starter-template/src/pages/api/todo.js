185952578395966f175dc6f713132b9ce6f9f0ec
Nate <foxyfan23@gmail.com>
2024-07-13 11:04:16 -0500

    starting page structure

--- starter-template/src/pages/api/todo.js
+++ starter-template/src/pages/api/todo.js
@@ -1,8 +1,9 @@
 import { prisma } from "@/server/prisma";
 
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({ message: "Hello World!" });
+    if (req.method === "GET") {
+        const todoItems = await prisma.todo.findMany()
+        res.status(200).json({ todo: todoItems });
   } else {
     res.status(404).json({ message: "We only support GET requests" });
   }
